ac349d3a6b9dabd4b0508b26ad7b4b1c06dba17b
Andris Andersons <andersonsandris890@gmail.com>
2022-11-28 19:51:24 +0200

    pamati

--- code/main.py
+++ code/main.py
@@ -1,1 +1,25 @@
-print("Tests")
+import pygame, sys
+from settings import *
+
+class Game:
+    def __init__(self):
+
+        #pamata setup
+        pygame.init()
+        self.screen = pygame.display.set.mode((WIDTH, HEIGHT))
+        self.clock = pygame = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
