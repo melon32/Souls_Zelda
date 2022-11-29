e54f6581143499d956d63e76f6198b45701f6f37
Andris Andersons <andersonsandris890@gmail.com>
2022-11-29 14:20:17 +0200

    Colision

--- code/level.py
+++ code/level.py
@@ -2,14 +2,16 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 class Level:
     def __init__(self):
         
         #get the display surface
         self.display_surface = pygame.display.get_surface()
+        
         #sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         #sprite setup
         self.create_map()
@@ -20,10 +22,12 @@ class Level:
                 x = col_index * TILESIZE
                 y = row_index * TILESIZE
                 if col == 'x':
-                    Tile((x, y), [self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
                 if col == 'p':
-                    Player((x, y), [self.visible_sprites])
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+
     def run(self):
         #update and draw the game
         self.visible_sprites.draw(self.display_surface)
-
+        self.visible_sprites.update()
+        debug(self.player.direction)
